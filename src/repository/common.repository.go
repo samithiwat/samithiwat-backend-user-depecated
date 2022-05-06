4d0305a101d4b6c7fe67b834e12790526767d808
samithiwat <samithiwat2544@gmail.com>
2022-05-06 21:18:11 +0700

    Implement cache method

--- src/repository/common.repository.go
+++ src/repository/common.repository.go
@@ -1,9 +1,13 @@
 package repository
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
 	"github.com/samithiwat/samithiwat-backend-user/src/proto"
 	"gorm.io/gorm"
 	"math"
+	"time"
 )
 
 func Pagination(value interface{}, meta *proto.PaginationMetadata) func(db *gorm.DB) *gorm.DB {
@@ -29,4 +33,28 @@ func Pagination(value interface{}, meta *proto.PaginationMetadata) func(db *gorm
 		offset := (meta.CurrentPage - 1) * meta.ItemsPerPage
 		return db.Offset(int(offset)).Limit(int(meta.ItemsPerPage))
 	}
+}
+
+func SaveCache(c *redis.Client, key string, value interface{}) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	v, err := json.Marshal(value)
+	if err != nil {
+		return
+	}
+
+	return c.Set(ctx, key, v, 0).Err()
+}
+
+func GetCache(c *redis.Client, key string, value interface{}) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	v, err := c.Get(ctx, key).Result()
+	if err != nil {
+		return
+	}
+
+	return json.Unmarshal([]byte(v), value)
 }
