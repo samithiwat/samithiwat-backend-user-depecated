e70fc24b5dd9eb78793a209e35362179c40a0ddb
samithiwat <samithiwat2544@gmail.com>
2022-05-06 21:17:52 +0700

    Add user model to auto migrate

--- src/database/mysql.connection.go
+++ src/database/mysql.connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"github.com/samithiwat/samithiwat-backend-user/src/config"
+	"github.com/samithiwat/samithiwat-backend-user/src/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"strconv"
@@ -16,7 +17,7 @@ func InitDatabase(conf *config.Database) (gormDb *gorm.DB, err error) {
 		return
 	}
 
-	err = gormDb.AutoMigrate()
+	err = gormDb.AutoMigrate(model.User{})
 	if err != nil {
 		return
 	}
