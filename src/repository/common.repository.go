da258d6f172adc4dd6cda8afc2d509f2450ed1e7
samithiwat <samithiwat2544@gmail.com>
2022-05-08 13:05:00 +0700

    Refactor query code

--- src/repository/common.repository.go
+++ src/repository/common.repository.go
@@ -4,34 +4,22 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-redis/redis/v8"
-	"github.com/samithiwat/samithiwat-backend-user/src/proto"
+	"github.com/samithiwat/samithiwat-backend-user/src/model"
 	"gorm.io/gorm"
 	"math"
 	"time"
 )
 
-func Pagination(value interface{}, meta *proto.PaginationMetadata) func(db *gorm.DB) *gorm.DB {
+func Pagination(value interface{}, meta *model.PaginationMetadata, db *gorm.DB) func(db *gorm.DB) *gorm.DB {
+	var totalItems int64
+	db.Model(&value).Count(&totalItems)
+
+	meta.TotalItem = totalItems
+	totalPages := math.Ceil(float64(totalItems) / float64(meta.ItemsPerPage))
+	meta.TotalPage = int64(totalPages)
+
 	return func(db *gorm.DB) *gorm.DB {
-		var totalItems int64
-		db.Model(value).Count(&totalItems)
-
-		meta.TotalItem = totalItems
-		totalPages := math.Ceil(float64(totalItems) / float64(meta.ItemsPerPage))
-		meta.TotalPage = int64(totalPages)
-
-		if meta.CurrentPage < 1 {
-			meta.CurrentPage = 1
-		}
-
-		switch {
-		case meta.ItemsPerPage > 100:
-			meta.ItemsPerPage = 100
-		case meta.ItemsPerPage < 10:
-			meta.ItemsPerPage = 10
-		}
-
-		offset := (meta.CurrentPage - 1) * meta.ItemsPerPage
-		return db.Offset(int(offset)).Limit(int(meta.ItemsPerPage))
+		return db.Offset(int(meta.GetOffset())).Limit(int(meta.ItemsPerPage))
 	}
 }
 
@@ -44,7 +32,7 @@ func SaveCache(c *redis.Client, key string, value interface{}) (err error) {
 		return
 	}
 
-	return c.Set(ctx, key, v, 0).Err()
+	return c.Set(ctx, key, v, 5*time.Second).Err()
 }
 
 func GetCache(c *redis.Client, key string, value interface{}) (err error) {
