d14eb54a9a63a8c07b9ee4cadc8e0517b62dd416
samithiwat <samithiwat2544@gmail.com>
2022-05-09 07:37:02 +0700

    Reschema of config file

--- src/config/config.go
+++ src/config/config.go
@@ -7,7 +7,6 @@ import (
 
 type Database struct {
 	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
 	User     string `mapstructure:"username"`
 	Password string `mapstructure:"password"`
 	Name     string `mapstructure:"name"`
